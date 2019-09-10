3f47dad2e485ccd3f9bfdcab7cddc8dd1bc89b8b
andrewminai24 <andrewminai24@gmail.com>
2019-09-10 12:27:05 -0700

    project1

--- Project1/Driver.java
+++ Project1/Driver.java
@@ -1,3 +1,20 @@
+/*
+Write a program to have a Car class which is comparable.
+ Make the Car class comparable and use speed to compare cars. 
+Write a method in Main class that finds the minimum of 4 things (generically).
+ Create 4 cars and pass the cars to the minimum method and find the smallest car. 
+ Have a toString method for the Car class. 
+ The main program should be able to find out how many cars are created so far by calling a static method called countCars.
+ Create another class called LuxCar that extends the class Car. 
+ You may have an additional member variable called airCondition of type intger (with setters and getters). 
+ Make the variables in Car class protected. 
+ Create 4 LuxCar objects and find the smallest one.
+  Does LuxCar objects work with minimum method?
+  
+  due september12th
+*/
+
+
 
 public class Driver {
 

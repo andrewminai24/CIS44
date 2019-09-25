102cc70dce7b04482d2a7ae58a21fe90b8ace9d8
andrewminai24 <andrewminai24@gmail.com>
2019-09-25 02:13:23 -0700

    linkedlist to Array

---
+++ Prep/Linkedlists/LinktoArray.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class LinktoArray{
+    public static void main(String[] args){
+        LinkedList list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addFirst(5);
+        list.contains(10);
+        System.out.println(list.contains(10));
+        System.out.println(list.indexOf(10));
+        System.out.println(list.size());
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
+    
+    }
+    
+}

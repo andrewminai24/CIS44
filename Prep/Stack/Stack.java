bb97be61971bca5cd2e3460d50ebebcd4945f090
andrewminai24 <andrewminai24@gmail.com>
2019-10-02 02:57:13 -0700

    stack implementation

---
+++ Prep/Stack/Stack.java
@@ -0,0 +1,26 @@
+/*
+
+what is postfix for a+b
+
+(a*b)/(c-d)
+(ab*)/cd-)
+(ab*)(cd-)/
+ab*cd-/
+
+question-4 above part d
+a/b + c-d
+The operation that come first has higher prority
+stack is a linear data structure which follows a 
+particular order in which the operations are performed.The
+order may be LIFO(Last in First Out) (First in Last Out)
+
+Mainly the following three basic operations are performed 
+in the stack:
+
+	Push Adds an item to the stack 
+	if the stack is full
+	
+	Pop removes the item from the stack.
+
+	Peek or TOp
+*/

4621eae8b304508dc399b9021210b3ec9efd73f7
andrewminai24 <andrewminai24@gmail.com>
2019-11-05 14:15:31 -0800

    lab5

---
+++ Lab5/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+
+    
+    public static void main(String[] args){
+        Integer[] aInteger = {5,9,1,3};
+        System.out.println("Before");
+        display(arrInteger,4);
+        s.quicksort(arrInteger,4);
+        System.out.println("And after:");
+        
+        display(arrInteger,4);
+        QuickSort.quickSort(arrInteger,4);
+
+    }
+}

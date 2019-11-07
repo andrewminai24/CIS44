29a31f133e884510095daae928b53864d3eb1545
andrewminai24 <andrewminai24@gmail.com>
2019-11-07 02:28:45 -0800

    Complexity analysis

---
+++ Prep/Merge.java
@@ -0,0 +1,13 @@
+/*
+Merge Sort
+
+1.In Merge sort, the idea of the algorithm is to sort smaller arrays
+and then combine those arrays together (merge them) in sorted order.
+
+2.Merge sort leverages recursion
+
+Pseudocode:
+1.Sort the left half of the array(assuming n > 1)
+2.Sort the right half of the array(assuming n > 1)
+3.Merge the two halves together
+*/

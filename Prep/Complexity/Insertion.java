f77324f228c030832e7b0913c4c4b844cf9ef878
andrewminai24 <andrewminai24@gmail.com>
2019-11-07 02:42:49 -0800

    Complexity analysis

---
+++ Prep/Complexity/Insertion.java
@@ -0,0 +1,22 @@
+/*
+Running time of insertion sort
+worst case:
+T(n) = an^2 + bn + c
+Counting the number of operations
+
+Do not count the number of lines
+An algorithm that is asymptotically more efficent
+will be the best choice for all n
+
+a.f(n) = 6n^2 + 3 is O(n^2)
+b.n^2 + 17n + 1 is O(n^2)
+
+f(n) = 6n^2 + 3
+g(n) = n^2
+6n^2 + 3 <= cn^2
+
+*/
+
+public class Insertion{
+
+}

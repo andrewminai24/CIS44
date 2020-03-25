0830622373b7d77cc3f0afd6867dedfcf56f296d
Andrewminai24 <andrewminai24@gmail.com>
2020-03-24 21:37:11 -0700

    ip address

---
+++ leetcode/ip/IpAddress.java
@@ -0,0 +1,19 @@
+public class IpAddress{
+    public static void main(String[]args){
+
+    }
+    public static String Address(String x){
+       StringBuilder result = new Stringbuilder();
+       for(int i = 0; i < x.length(); i++){
+           char current = x.charAt(i);
+            if(current == "."){
+                result.append("[.]")
+            }else{
+                return result.append(current);
+            }
+
+       }
+       return result.toString();
+    }
+}
+

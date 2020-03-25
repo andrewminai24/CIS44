6e5a022fa01d0a3373b9c9cf34977244de7d9419
Andrewminai24 <andrewminai24@gmail.com>
2020-03-24 21:37:36 -0700

    ip address

--- leetcode/ip/IpAddress.java
+++ leetcode/ip/IpAddress.java
@@ -7,7 +7,7 @@ public class IpAddress{
        for(int i = 0; i < x.length(); i++){
            char current = x.charAt(i);
             if(current == "."){
-                result.append("[.]")
+                result.append("[.]");
             }else{
                 return result.append(current);
             }
